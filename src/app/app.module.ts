ddddd69dc40432da6fd163af8a344e69c63e3ce1
kbuczynski <nash25pl@gmail.com>
2020-08-02 22:01:54 +0100

    -add font awsome and angular material lib -add rest of element and social button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule }                 from './app-routing.module';
+import { AppComponent }                     from './app.component';
+import { LoginPageComponent }               from './components/login-page/login-page.component';
+import { NgbModule }                        from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule }          from '@angular/platform-browser/animations';
+import { MatFormFieldModule }               from '@angular/material/form-field';
+import { MatInputModule }                   from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule }                    from "@angular/material/icon";
+import { MatButtonModule }                  from "@angular/material/button";
+import { MatCheckboxModule }                from "@angular/material/checkbox";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule(
+  {
+    declarations: [
+      AppComponent,
+      LoginPageComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NgbModule,
+      BrowserAnimationsModule,
+      MatFormFieldModule,
+      MatInputModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MatIconModule,
+      MatButtonModule,
+      MatCheckboxModule,
+      FontAwesomeModule,
+    ],
+    providers:    [],
+    bootstrap:    [AppComponent]
+  }
+)
+export class AppModule {
+}
--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }       from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
+import { faGoogle }                from "@fortawesome/free-brands-svg-icons";
 
-@Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
-})
+@Component(
+  {
+    selector:    'login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls:   ['./login-page.component.scss']
+  }
+)
 export class LoginPageComponent implements OnInit {
+  email: FormControl;
+  password: string;
+  hide: boolean;
+  rememberPassoword: boolean;
 
-  constructor() { }
+  faGoogle = faGoogle;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.hide  = true;
+    this.rememberPassoword = false;
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]);
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
